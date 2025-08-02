dc1f12c968d8bd045618b518b863e24ed36edd9e
Tanay Dutta <tanay.dutta881@gmail.com>
2025-08-02 12:34:12 +0530

    Update resume

--- frontend/src/components/ProjectSection.jsx
+++ frontend/src/components/ProjectSection.jsx
@@ -4,7 +4,7 @@ const projects = [
   {
     title: "BloodConnect",
     image: "https://images.unsplash.com/photo-1585673551689-74e88de97395?q=80&w=2679&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    link: "https://github.com/yourusername/blog-platform",
+    link: "https://github.com/tanay881/BloodConnect.git",
   },
   {
     title: "E-Commerce Store",
@@ -14,7 +14,7 @@ const projects = [
   {
     title: "Portfolio Website",
     image: "https://images.unsplash.com/photo-1580813464570-f36c6aaa0859?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    link: "https://github.com/yourusername/portfolio",
+    link: "https://github.com/tanay881/portfolio.git",
   },
   {
     title: "Wanderlust",
